1e2ad5090cd27f1c8622e27a97fa76a926b626de
James Rea <jamesrea97@gmail.com>
2024-11-05 10:47:01 +0000

    Add flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A flake for mach-o-linking.";
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        cargoBuildInputs = with pkgs; lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.CoreServices
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            clippy
+          ] ++ cargoBuildInputs;
+          packages = [ pkgs.git ];
+        };
+      }
+    );
+}
